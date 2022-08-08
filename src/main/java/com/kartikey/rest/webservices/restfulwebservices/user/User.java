8b622e95f88d35a8a9e3a41949177494344fc34a
Kartikey Mishr <kartikeymishr@hotmail.com>
2022-08-08 17:18:50 +0530

    Adding JPA + H2. Changing Static User Entity to JPA Entity.

--- src/main/java/com/kartikey/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/kartikey/rest/webservices/restfulwebservices/user/User.java
@@ -1,15 +1,17 @@
 package com.kartikey.rest.webservices.restfulwebservices.user;
 
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
-//@Entity
+
+@Entity
 public class User {
-//    @Id
-//    @GeneratedValue
+
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2, message = "Name should have at least 2 characters.")
     private String name;
